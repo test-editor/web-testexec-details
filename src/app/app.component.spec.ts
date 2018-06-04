2b9f6115317789fbe296b6c7ec06a0652e3085ae
Jan Jelschen <jan.jelschen@itemis.de>
2018-06-04 10:54:07 +0200

    fix tests / add component declarations in tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { TestExecDetailsComponent } from './modules/details/test-exec-details.component';
+import { PropertiesViewComponent } from './modules/properties/properties-view.component';
+import { By } from '@angular/platform-browser';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        TestExecDetailsComponent
+        TestExecDetailsComponent,
+        PropertiesViewComponent
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render sub-component', async(() => {
+    // given
     const fixture = TestBed.createComponent(AppComponent);
+
+    // when
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('div').textContent).toContain('details view works');
+
+    // then
+    const nestedDetailsComponent = fixture.debugElement.query(By.css('app-test-exec-details'));
+    expect(nestedDetailsComponent).not.toBeNull();
   }));
 });
