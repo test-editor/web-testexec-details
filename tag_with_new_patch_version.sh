caa1df769e0d8aac7ace7c45f130a0bb6e932c6d
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-07-03 17:10:13 +0200

    tag only if the last commit author was not that tagging author

--- tag_with_new_patch_version.sh
+++ tag_with_new_patch_version.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-echo "tag with new patch level version, since publishing is wanted"
-old_version=`npm view @testeditor/testexec-details version`
-echo "old version (before tagging) was v$old_version"
-if [ "$GH_EMAIL" == "" -o "$GH_TOKEN" == "" ]; then
-  echo "tagging is not done since email and token for push into github is missing!"
+tag_author="srvte tagging"
+last_author=`git show --format="%aN" HEAD | head -n 1`
+echo "Last author was: $last_author"
+if [ "$last_author" == "$tag_author" ]; then
+  echo "Author of last commit seems to be the one that is reserved for pushing tags."
+  echo "No new version is tagged!"
 else
-  # configure for git push to work automatically
-  github_project=`git config --get remote.origin.url | sed 's|.*\(/[^/]*/[^/]*\)$|\1|g'`
-  git config user.name "srvte"
-  git config user.email "$GH_EMAIL"
-  git remote remove origin || true
-  git remote add origin https://$GH_TOKEN@github.com$github_project
-  git remote -v # show the now configured remotes
-  git checkout - # if detached, try to return to a regular branch
-  git fetch # necessary to make origin/master known
-  git branch --set-upstream-to=origin/master
-  git remote -v # show the now configured remotes
-  git status # show some info
-  git tag # show tag info
-  npm version patch # create git commit and tag automatically!
-  # postversion action in package.json will execute git push && git push --tags
-  new_version=`npm view @testeditor/testexec-details version`
-  echo "tagged now with v$new_version"
+  echo "tag with new patch level version, since publishing is wanted"
+  old_version=`npm view @testeditor/testexec-details version`
+  echo "old version (before tagging) was v$old_version"
+  if [ "$GH_EMAIL" == "" -o "$GH_TOKEN" == "" ]; then
+    echo "tagging is not done since email and token for push into github is missing!"
+  else
+    # configure for git push to work automatically
+    github_project=`git config --get remote.origin.url | sed 's|.*\(/[^/]*/[^/]*\)$|\1|g'`
+    git config user.name "$tag_author"
+    git config user.email "$GH_EMAIL"
+    git remote remove origin || true
+    git remote add origin https://$GH_TOKEN@github.com$github_project
+    git remote -v # show the now configured remotes
+    git checkout - # if detached, try to return to a regular branch
+    git fetch # necessary to make origin/master known
+    git branch --set-upstream-to=origin/master
+    git remote -v # show the now configured remotes
+    git status # show some info
+    git tag # show tag info
+    npm version patch # create git commit and tag automatically!
+    # postversion action in package.json will execute git push && git push --tags
+    new_version=`npm view @testeditor/testexec-details version`
+    echo "tagged now with v$new_version"
+  fi
 fi
