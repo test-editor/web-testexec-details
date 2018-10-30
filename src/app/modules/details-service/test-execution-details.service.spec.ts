60782ecff2a81f85c493fcec09731f5323116749
Jan Jelschen <jan.jelschen@itemis.de>
2018-10-30 10:06:56 +0100

    extend service interface to allow requesting logs filtered by level

--- src/app/modules/details-service/test-execution-details.service.spec.ts
+++ src/app/modules/details-service/test-execution-details.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, inject, fakeAsync, tick, flush } from '@angular/core/testing';
 
 import { TestExecutionDetailsService, DefaultTestExecutionDetailsService,
-  TestExecutionDetails, DataKind } from './test-execution-details.service';
+  TestExecutionDetails, DataKind, LogLevel } from './test-execution-details.service';
 import { TestExecutionDetailsServiceConfig } from './test-execution-details-service-config';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
@@ -58,6 +58,27 @@ describe('TestExecutionDetailsService', () => {
 
     // when
     service.getTestExecutionDetails(id).
+
+    // then
+    then((details) => {
+      expect(details).toEqual(mockResponse);
+    });
+    tick();
+    httpController.match(testExecutionRequest)[0].flush(mockResponse);
+  })));
+
+  it('appends the log level as query parameter, if provided', fakeAsync(inject([HttpTestingController, TestExecutionDetailsService],
+    (httpController: HttpTestingController, service: TestExecutionDetailsService) => {
+    // given
+    const id = '42/1/2/23';
+    const testExecutionRequest = {
+      method: 'GET',
+      url: serviceConfig.url + '/42/1/2/23?logLevel=INFO'
+    };
+    const mockResponse: TestExecutionDetails[] = [{ type: DataKind.text, content: ['Hello World!', 'This can contain multiple lines.'] }];
+
+    // when
+    service.getTestExecutionDetails(id, LogLevel.INFO).
 
     // then
     then((details) => {
