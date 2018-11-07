c7e76a440929ec852280237f39a5bcc703ed3b46
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-11-07 16:11:44 +0100

    react to tree reloads and check ids before getting details

--- src/app/modules/event-types.ts
+++ src/app/modules/event-types.ts
@@ -3,3 +3,4 @@
 export const TEST_NAVIGATION_SELECT = 'test.navigation.select';
 export const HTTP_CLIENT_NEEDED = 'httpClient.needed';
 export const HTTP_CLIENT_SUPPLIED = 'httpClient.supplied';
+export const TEST_EXECUTION_TREE_LOADED = 'test.execution.tree.loaded';
