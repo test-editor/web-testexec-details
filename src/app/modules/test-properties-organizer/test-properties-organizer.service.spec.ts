0f65799e7fabd8ea75e279f3f323e29575787cc6
Jan Jelschen <jan.jelschen@itemis.de>
2019-03-26 16:55:27 +0100

    implement test properties organizer service

--- src/app/modules/test-properties-organizer/test-properties-organizer.service.spec.ts
+++ src/app/modules/test-properties-organizer/test-properties-organizer.service.spec.ts
@@ -1,15 +1,54 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { inject, TestBed } from '@angular/core/testing';
+import { TestPropertiesOrganizerServiceConfig } from './test-properties-organizer-service-config';
 import { TestPropertiesOrganizerService } from './test-properties-organizer.service';
 
+
 describe('TestPropertiesOrganizerService', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TestPropertiesOrganizerService]
+      providers: [TestPropertiesOrganizerService,
+        { provide: TestPropertiesOrganizerServiceConfig, useValue: { propertyPriorityMap: {} } }]
     });
   });
 
   it('should be created', inject([TestPropertiesOrganizerService], (service: TestPropertiesOrganizerService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should sort by alphabet if no priorities are set',
+    inject([TestPropertiesOrganizerService, TestPropertiesOrganizerServiceConfig],
+      (service: TestPropertiesOrganizerService, config: TestPropertiesOrganizerServiceConfig) => {
+        // given
+        const unsorted = ['zeta', 'gamma', 'alpha', 'omega', 'lambda', 'beta', 'delta', 'epsilon'];
+        const sorted = ['alpha', 'beta', 'delta', 'epsilon', 'gamma', 'lambda', 'omega', 'zeta'];
+
+        // when
+        const actual = service.organize(unsorted);
+
+        // then
+        expect(actual).toEqual(sorted);
+        expect(unsorted).not.toEqual(sorted);
+      }));
+
+  it('should put buckets with higher index to the top, and sort each bucket alphabetically',
+    inject([TestPropertiesOrganizerService, TestPropertiesOrganizerServiceConfig],
+      (service: TestPropertiesOrganizerService, config: TestPropertiesOrganizerServiceConfig) => {
+        // given
+        config.propertyPriorityMap['zeta'] = 100;
+        config.propertyPriorityMap['delta'] = 100;
+        config.propertyPriorityMap['omega'] = 101;
+        config.propertyPriorityMap['epsilon'] = 0;
+        config.propertyPriorityMap['beta'] = -42;
+        const unsorted = ['zeta', 'gamma', 'alpha', 'omega', 'lambda', 'beta', 'delta', 'epsilon'];
+        const sorted = ['omega', 'delta', 'zeta', 'alpha', 'epsilon', 'gamma', 'lambda', 'beta'];
+
+
+        // when
+        const actual = service.organize(unsorted);
+
+        // then
+        expect(actual).toEqual(sorted);
+        expect(unsorted).not.toEqual(sorted);
+      }));
 });
--- src/app/modules/test-properties-organizer/test-properties-organizer.service.ts
+++ src/app/modules/test-properties-organizer/test-properties-organizer.service.ts
@@ -5,13 +5,47 @@ export abstract class PropertiesOrganizerService {
   abstract organize(propertyNames: string[]): string[];
 }
 
+type BucketList = Record<number, string[]>;
+
 @Injectable()
 export class TestPropertiesOrganizerService implements PropertiesOrganizerService {
 
-constructor(private config: TestPropertiesOrganizerServiceConfig) {}
+  constructor(private config: TestPropertiesOrganizerServiceConfig) { }
 
   organize(propertyNames: string[]): string[] {
-    throw new Error('Method not implemented.');
+    const result: string[] = [];
+    const bucketLists = this.bucketsFrom(propertyNames);
+    this.bucketsByPriority().forEach((index) => result.push(...(bucketLists[index].sort())));
+    return result;
+  }
+
+  bucketsFrom(propertyNames: string[]): BucketList {
+    const result: BucketList = {};
+    propertyNames.forEach((name) => {
+      const bucket = this.bucketOf(name);
+      if (result[bucket]) {
+        result[bucket].push(name);
+      } else {
+        result[bucket] = [name];
+      }
+    })
+    return result;
+  }
+
+  bucketOf(name: string): number {
+    if (this.config.propertyPriorityMap[name]) {
+      return this.config.propertyPriorityMap[name];
+    } else {
+      return 0;
+    }
+  }
+
+  bucketsByPriority(): Set<number> {
+    return new Set<number>(
+      Object.values(this.config.propertyPriorityMap)
+        .concat([0])
+        .sort((index1, index2) => index2 - index1)
+    );
   }
 
 }
