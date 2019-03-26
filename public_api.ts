259be36a6aa3b1c0e2f51694e607dd4fb2fb7989
Jan Jelschen <jan.jelschen@itemis.de>
2019-03-26 17:45:12 +0100

    use organize service to sort properties

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,2 @@
-export * from './src/app/modules/details/test-exec-details.module'
+export * from './src/app/modules/details/test-exec-details.module';
+export { TestPropertiesOrganizerServiceConfig } from './src/app/modules/test-properties-organizer/test-properties-organizer-service-config';
--- src/app/modules/details/test-exec-details.module.ts
+++ src/app/modules/details/test-exec-details.module.ts
@@ -1,21 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DefaultWindowService, WindowService } from '@testeditor/testeditor-commons';
+import { DefaultWindowService, HttpProviderService, WindowService } from '@testeditor/testeditor-commons';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { TestExecutionDetailsServiceConfig } from '../details-service/test-execution-details-service-config';
 import { DefaultTestExecutionDetailsService, TestExecutionDetailsService } from '../details-service/test-execution-details.service';
-import { HttpProviderService } from '@testeditor/testeditor-commons';
 import { PropertiesViewModule } from '../properties/properties-view.module';
 import { ResourceServiceConfig } from '../resource-service/resource-service-config';
 import { DefaultResourceService, ResourceService } from '../resource-service/resource.service';
-import { PropertiesPrettifierService,
-  TestPropertiesPrettifierService } from '../test-properties-prettifier/test-properties-prettifier.service';
-import { DefaultFileReaderProvider, FileReaderProvider, TestExecDetailsComponent } from './test-exec-details.component';
-import { TestPropertiesOrganizerService, PropertiesOrganizerService } from '../test-properties-organizer/test-properties-organizer.service';
 import { TestPropertiesOrganizerServiceConfig } from '../test-properties-organizer/test-properties-organizer-service-config';
+import { PropertiesOrganizerService, TestPropertiesOrganizerService } from '../test-properties-organizer/test-properties-organizer.service';
+import {
+  PropertiesPrettifierService, TestPropertiesPrettifierService
+} from '../test-properties-prettifier/test-properties-prettifier.service';
+import { DefaultFileReaderProvider, FileReaderProvider, TestExecDetailsComponent } from './test-exec-details.component';
 
 @NgModule({
   imports: [
@@ -29,8 +29,8 @@ import { TestPropertiesOrganizerServiceConfig } from '../test-properties-organiz
   ],
   providers: [
     HttpProviderService,
-    {provide: TestExecutionDetailsService, useClass: DefaultTestExecutionDetailsService},
-    {provide: ResourceService, useClass: DefaultResourceService},
+    { provide: TestExecutionDetailsService, useClass: DefaultTestExecutionDetailsService },
+    { provide: ResourceService, useClass: DefaultResourceService },
     { provide: FileReaderProvider, useClass: DefaultFileReaderProvider },
     { provide: WindowService, useClass: DefaultWindowService },
     { provide: PropertiesPrettifierService, useClass: TestPropertiesPrettifierService },
@@ -40,12 +40,12 @@ import { TestPropertiesOrganizerServiceConfig } from '../test-properties-organiz
 export class TestExecDetailsModule {
   static forRoot(detailsServiceConfig: TestExecutionDetailsServiceConfig,
     resourceServiceConfig: ResourceServiceConfig,
-    propertiesOrganizerServiceConfig?: TestPropertiesOrganizerServiceConfig): ModuleWithProviders {
+    propertiesOrganizerServiceConfig: TestPropertiesOrganizerServiceConfig = { propertyPriorityMap: {} }): ModuleWithProviders {
     return {
       ngModule: TestExecDetailsModule,
-      providers: [ { provide: TestExecutionDetailsServiceConfig, useValue: detailsServiceConfig },
-                   { provide: ResourceServiceConfig, useValue: resourceServiceConfig },
-                   { provide: TestPropertiesOrganizerServiceConfig, useValue: propertiesOrganizerServiceConfig} ]
+      providers: [{ provide: TestExecutionDetailsServiceConfig, useValue: detailsServiceConfig },
+      { provide: ResourceServiceConfig, useValue: resourceServiceConfig },
+      { provide: TestPropertiesOrganizerServiceConfig, useValue: propertiesOrganizerServiceConfig }]
     };
   }
 }
